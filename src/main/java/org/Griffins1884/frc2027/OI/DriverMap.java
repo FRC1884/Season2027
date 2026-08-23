675896decc01d43c31fcab2a46d8a2317a695e47
JonathanSt-Georges___ <JonathanStGeorges@icloud.com>
2026-08-23 10:14:51 +0100

    refactor: carry forward mechanism-free driver map

---
+++ src/main/java/org/Griffins1884/frc2027/OI/DriverMap.java
@@ -0,0 +1,29 @@
+package org.Griffins1884.frc2027.OI;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import java.util.function.DoubleSupplier;
+
+/** Reusable driver-input abstraction carried forward from Season2026 without mechanism bindings. */
+public interface DriverMap {
+    DoubleSupplier getXAxis();
+
+    DoubleSupplier getYAxis();
+
+    DoubleSupplier getRotAxis();
+
+    Trigger resetOdometry();
+
+    default Trigger leftBackButton() {
+        return new Trigger(() -> false);
+    }
+
+    default Trigger rightBackButton() {
+        return new Trigger(() -> false);
+    }
+
+    default Command rumble() {
+        return Commands.none();
+    }
+}
